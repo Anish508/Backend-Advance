d4a6c4603387ccc7702f0953faadaa0377b41877
Anish508 <anishbarke9741@gmail.com>
2025-07-29 17:37:49 +0530

    Performing CRUD operation in Postgres

--- POSTGRESS-CONCEPTS/src/concepts/basic_query.js
+++ POSTGRESS-CONCEPTS/src/concepts/basic_query.js
@@ -11,7 +11,7 @@ async function createUserTable() {
   `;
 
       try {
-            db.query(createTableQuery)
+            await db.query(createTableQuery)
             console.log(`User table cretaed successfully`);
 
       } catch (error) {
@@ -20,4 +20,77 @@ async function createUserTable() {
       }
 }
 
-module.exports = { createUserTable }
+async function insertUser(username, email) {
+  const insertQuery = `
+    INSERT INTO users (username, email) 
+    VALUES ($1, $2)
+    RETURNING *
+  `;
+
+  try {
+    const res = await db.query(insertQuery, [username, email]);
+    console.log(`User inserted successfully`, res.rows[0]);
+    return res.rows[0];
+  } catch (error) {
+    console.log(error, "Error while inserting user");
+    throw error; // Optional: rethrow to handle it at higher level
+  }
+}
+
+async function fetchAllUsers() {
+      const getAllUserQuery = `
+      SELECT * FROM users
+      `
+
+      try {
+            const res = await db.query(getAllUserQuery)
+            console.log("All users details:")
+            return res.rows
+      } catch (error) {
+            console.log("Error while fetching record", error);
+            throw error
+      }
+}
+
+async function updateUserEmail(username , newEmail) {
+      const updateUserQuery = `
+      UPDATE users SET 
+      email = $2
+      WHERE username = $1
+      RETURNING *
+      `
+
+      try {
+            const res = await db.query(updateUserQuery, [username, newEmail])
+            if(res.rows.length > 0){
+                  console.log(`user updated successfully`, res.rows[0]);
+                  return res.rows[0]
+            }else{
+                  console.log("No user to update the email");
+                  
+            }
+      } catch (error) {
+            console.log("Error while updating record",error );
+            throw error
+      }
+}
+
+async function deleteUserInfo(username) {
+      const deleteUserQuery =`
+      DELETE FROM users
+      WHERE username = $1
+      RETURNING *
+      `
+      try {
+            const res = await db.query(deleteUserQuery, [username])
+            
+                  console.log(`user deleted successfully`, res.rows[0]);
+                  return res.rows[0]
+           
+            
+      } catch (error) {
+            console.log("Error while deleting user record", error);
+            return error
+      }
+}
+module.exports = { createUserTable, insertUser, fetchAllUsers , updateUserEmail, deleteUserInfo}
--- POSTGRESS-CONCEPTS/src/server.js
+++ POSTGRESS-CONCEPTS/src/server.js
@@ -1,11 +1,27 @@
 
-const {createUserTable}= require('./concepts/basic_query.js')
+const {createUserTable, insertUser, fetchAllUsers, updateUserEmail, deleteUserInfo}= require('./concepts/basic_query.js')
 
 //testing basic queries
 
 async function testBasicQueries() {
       try {
             await createUserTable()
+
+            //insert users
+            / await insertUser('Anish', 'Anishbarke@gmail.com')
+            /* await insertUser('Manish', 'manish@gmail.com') */
+            await insertUser('visnish', 'vinish@gmail.com')
+            await insertUser('karan', 'karan@gmail.com') 
+            /* await insertUser('Ganesh', 'Ganesh@gmail.com') */
+
+          const allUser =  await fetchAllUsers()
+           console.log(allUser);
+           
+           updateUserEmail("Ganesh", "gandu@gmail.com")
+
+           const DeletedUserdata =await deleteUserInfo("Anish")
+           console.log(DeletedUserdata.rows); */
+           
       } catch (error) {
             console.log("Error:", error);
             
