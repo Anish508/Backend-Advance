319846c795ea29ffccbf4e1d8c90c310f6fd225b
Anish508 <anishbarke9741@gmail.com>
2025-08-05 16:18:35 +0530

    fixing the errors in prometheus

--- Prisma_Postgres/src/server.js
+++ Prisma_Postgres/src/server.js
@@ -12,20 +12,38 @@ const PORT = process.env.PORT ||3000
 const register = new promClient.Registry()
 promClient.collectDefaultMetrics({register})
 
-const httpRequests = new promClient.Counter({
+const httpRequestsCounter = new promClient.Counter({
       name: "http_requests_total",
       help:"Total number of http requests",
       labelNames: ["method", "route", "status"]
 })
 
-register.registerMetric(httpRequests)
+register.registerMetric(httpRequestsCounter)
+
+//middleware to track api request
+app.use((req,res,next)=>{
+      res.on('finish',()=>{
+            httpRequestsCounter.inc({
+                  method: req.method,
+                  route: req.path,
+                  status: res.statusCode
+            })
+      })
+      next()
+})
 
 //EXPOSE the /metrics endpoint for prometheus
 
-app.get('/metrics', async(req, res)=>{
-      res.set('Content-Type', register.contentType)
-      res.send(await register.metrics)
-})
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.send(await register.metrics()); 
+  } catch (err) {
+    console.error('Error in /metrics:', err);
+    res.status(500).send(err.message);
+  }
+});
+
 app.use("/api/author", authorRoutes )
 app.use("/api/book", bookRoutes )
 
