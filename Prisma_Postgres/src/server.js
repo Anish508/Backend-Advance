87da40a06860886ca38ec1e75b54087c4cdedd33
Anish508 <anishbarke9741@gmail.com>
2025-08-05 13:34:54 +0530

    Working on prometheus and graphana services building and deploying docker containers

--- Prisma_Postgres/src/server.js
+++ Prisma_Postgres/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const authorRoutes= require('./routes/authorRoutes.js')
 const bookRoutes= require('./routes/bookRoutes.js')
+const promClient = require('prom-client')
 
 const app = express()
 
@@ -8,8 +9,27 @@ app.use(express.json())
 
 const PORT = process.env.PORT ||3000
 
+const register = new promClient.Registry()
+promClient.collectDefaultMetrics({register})
+
+const httpRequests = new promClient.Counter({
+      name: "http_requests_total",
+      help:"Total number of http requests",
+      labelNames: ["method", "route", "status"]
+})
+
+register.registerMetric(httpRequests)
+
+//EXPOSE the /metrics endpoint for prometheus
+
+app.get('/metrics', async(req, res)=>{
+      res.set('Content-Type', register.contentType)
+      res.send(await register.metrics)
+})
 app.use("/api/author", authorRoutes )
 app.use("/api/book", bookRoutes )
+
+
 
 app.listen(PORT, ()=>{
       console.log("Server is now running on port:",PORT);
